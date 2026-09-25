1b250cd0ec093d44f0a9ba1a68d9e72287091330
Raffa Salisbury <raffasalisbury@gmail.com>
2026-09-26 10:32:10 +1200

    Create history.js

---
+++ history.js
@@ -0,0 +1,24 @@
+var ctx = document.getElementById("canvas").getContext("2d");
+class history {
+    constructor(name, year, species, aggression, diet, habitat, lifespan, size, weight, speed, intelligence, population){
+        this.name = name;
+        this.year = year;
+        this.species = species;
+        this.aggression = aggression;
+        this.diet = diet;
+        this.habitat = habitat;
+        this.lifespan = lifespan;
+        this.size = size;
+        this.weight = weight;
+        this.speed = speed;
+        this.intelligence = intelligence;
+        this.population = population;
+    }
+    tellHistory(name, year, species, aggression, diet, habitat, lifespan, size, weight, speed, intelligence, population){
+    ctx.fillStyle = "black";
+    ctx.fillText("")
+    
+    
+    }
+
+}
