250675d69a8fb297ef69f78e93ce75a59ec9ea11
Raffa Salisbury <raffasalisbury@gmail.com>
2026-09-26 11:36:28 +1200

    Update history.js

--- history.js
+++ history.js
@@ -1,5 +1,15 @@
 //var ctx = document.getElementById("canvas").getContext("2d");
 //const canvas = document.getElementById("canvas");
+var vowels = ["a", "e", "i", "o", "u"];
+var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
+var speciesNames = [consonants[Math.floor(Math.random() * consonants.length)] +
+                    vowels[Math.floor(Math.random() * vowels.length)]
+                    + consonants[Math.floor(Math.random() * consonants.length)] 
+                    + consonants[Math.floor(Math.random() * consonants.length)]
+                    + vowels[Math.floor(Math.random() * vowels.length)]
+                    + consonants[Math.floor(Math.random() * consonants.length)]
+                    ]
+const capitalisedSpeciesNames = speciesNames[0].toUpperCase();
 
 //window.onload=startCanvas
 class history {
@@ -24,7 +34,7 @@ class history {
     }
 
 }
-const newStory = new history("", Math.random()*500000, "", "", "", "", Math.random()*200, Math.random()*300, 
+const newStory = new history(capitalisedSpeciesNames, Math.random()*500000, "", "", "", "", Math.random()*200, Math.random()*300, 
      Math.random()*200, "", "", Math.random()*1000000);
 newStory.tellHistory(newStory.name, Math.round(newStory.year), newStory.species, newStory.aggression, 
       newStory.diet, newStory.habitat, Math.round(newStory.lifespan), Math.round(newStory.size), Math.round(newStory.weight),
