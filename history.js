a53161bafe43a5bd374c3a3d6d219f55cf202c10
Raffa Salisbury <raffasalisbury@gmail.com>
2026-09-26 11:02:14 +1200

    Update history.js

--- history.js
+++ history.js
@@ -1,5 +1,7 @@
-var ctx = document.getElementById("canvas").getContext("2d");
-window.onload=startCanvas
+//var ctx = document.getElementById("canvas").getContext("2d");
+//const canvas = document.getElementById("canvas");
+
+//window.onload=startCanvas
 class history {
     constructor(name, year, species, aggression, diet, habitat, lifespan, size, weight, speed, intelligence, population){
         this.name = name;
@@ -16,10 +18,14 @@ class history {
         this.population = population;
     }
     tellHistory(name, year, species, aggression, diet, habitat, lifespan, size, weight, speed, intelligence, population){
-    ctx.fillStyle = "black";
-    ctx.fillText("")
-    
-    
+        console.log(name, year, species, aggression, diet, habitat, lifespan, size, weight, speed, intelligence, population);
+        
+        
     }
 
 }
+const newStory = new history("", Math.random()*500000, "", "", "", "", "", Math.random()*200, 
+      Math.random()*100, Math.random()*50, Math.random()*10, Math.random()*1000000);
+newStory.tellHistory(newStory.name, Math.round(newStory.year), newStory.species, newStory.aggression, 
+      newStory.diet, newStory.habitat, Math.round(newStory.lifespan), Math.round(newStory.size), newStory.weight,
+      newStory.speed, newStory.intelligence, Math.round(newStory.population))
