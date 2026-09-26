a1586c7e0859e35aa7b8342a3df8b18fbe8f7c97
TheTrueFax <thetruefax.dev@gmail.com>
2026-09-26 17:16:00 +1200

    ashiuafjiasf

--- voronoi.js
+++ voronoi.js
@@ -1,82 +1,64 @@
 import sharp from 'sharp'
 import seedrandom from 'seedrandom'
+import { GPU } from 'gpu.js'
+const gpu = new GPU();
 
-const width = 1080;
-const height = 1920;
+const width = 1920/3;
+const height = 1080/3;
+//const width = 300;
+//const height = 300;
 
-const baseImg = await sharp("assets/researcher.jpeg").resize(width,height).raw().toBuffer({ resolveWithObject: true });
+
+const baseImg = await sharp("continent_map.png").resize(width,height).raw().toBuffer({ resolveWithObject: true });
+console.log(baseImg.info)
 const baseImageBuffer = new Uint8ClampedArray(baseImg.data.buffer);
 
-function v2(x, y) {
-	return {x:x,y:y};
-}
-
-const distance = (a,b) => Math.hypot(b.x- a.x, b.y - a.y);
-
-function plot_rand_points_tree(point_count, depth) {
-	const points = [];
-	for (let x = 0; x < point_count; x++) {
-		const point = v2(Math.random(), Math.random());
-		point.index = x;
-		points.push(point);
-	}
-	return points;
-}
-
-function get_voronoi_area(points, position) {
-	return points.toSorted((a, b) => distance(a, position) - distance(b, position));
-}
-
-const points = plot_rand_points_tree(30, 10);
-
-function generate_voronoi(points, width, height) {
-	const pixelBuffer = Buffer.alloc(width * height);
-	for (let y = 0; y < height; y++) {
-		for (let x = 0; x < width; x++) {
-			const v = get_voronoi_area(points, v2(x/width, y/width));
-			pixelBuffer[y * width + x] = v[0].index;
+const cpic = gpu.createKernel(function(points, width, pointlength) {
+	const y = Math.floor(this.thread.x / width);
+	const x = this.thread.x % width;
+	let closest_index = 1;
+	let closest_dist = 999999999;
+	for (let i=0;i<pointlength/2;i++) {
+		let dx = x - points[i*2];
+		let dy = y - points[i*2+1];
+		const dist = Math.sqrt(dx*dx + dy*dy);
+		if (dist < closest_dist) {
+			closest_dist = dist;
+			closest_index = i;
 		}
 	}
-	return pixelBuffer;
-}
+	return closest_index
+}).setOutput([width * height]);
 
-function voronize_image(vimg, frame) {
-	const pixelBuffer = Buffer.alloc(width * height * 4);
-
-	//console.log(points)
-
-	for (let y = 0; y < height; y++) {
-		for (let x = 0; x < width; x++) {
-    			const index = (y * width + x) * 4;
-			//const v = get_voronoi_area(points, v2(x/width,y/height)) // sorted list of distance
-			/*if (distance(v2(x/height,y/height), v[0]) - distance(v2(x/height,y/height), v[1]) < 0.05) {
-				color = 255;
-			}*/
-			//if (distance(v[0], v2(x/width,y/height)) > 0.1) {
-			const generator = seedrandom(vimg[index/4]);
-			let new_x = parseInt(x + (generator()-0.5) * frame);
-			let new_y = parseInt(y + (generator()-0.5) * frame);
-			new_x += (new_x<0)?width:0;
-			new_y += (new_y<0)?height:0;
-			new_x -= (new_x>width)?width:0;
-			new_y -= (new_y>height)?height:0;
+const voronization = gpu.createKernel(function(vimg, image, offset, width, height, pointcount) {
+	const x_offset = Math.floor(Math.sin(vimg[Math.floor(this.thread.x/4)]/pointcount * 6.28)) * offset;
+	const y_offset = Math.floor(Math.cos(vimg[Math.floor(this.thread.x/4)]/pointcount * 6.28)) * offset;
 	
-	    		const index2 = (new_y * width + new_x) * 3;
-		
-			pixelBuffer[index] = baseImageBuffer[index2];
-			pixelBuffer[index+1] = baseImageBuffer[index2+1];
-			pixelBuffer[index+2] = baseImageBuffer[index2+2];
-			pixelBuffer[index+3] = 255;
-		}
-	}
-	return pixelBuffer;
+	const index = this.thread.x + x_offset*4 + y_offset*width*4;
+
+	index -= (index > width*height*4)?width*height*4:0;
+	index += (index < 0)?width*height*4:0;
+
+	return image[index];
+}).setOutput([width * height * 4]);
+
+const points = [];
+for (let i = 0;i<20;i++) {
+	points.push(Math.random()*width, Math.random()*height)
 }
 
-const voronoi = generate_voronoi(points, width, height);
+const voronoi = cpic(points,width,points.length);
 
-for (let x = 0; x<20; x++) {
+console.log(voronoi)
+
+//const rand = Math.random() * 1000
+
+for (let x = 0; x<50; x++) {
 	console.log("starting frame " +x)
-  	await sharp(voronize_image(voronoi, x), {
+	//console.log(baseImageBuffer)
+	//console.log(voronization(voronoi, baseImageBuffer, 1, width))
+	//voronization(voronoi, baseImageBuffer, x, width)
+  	await sharp(Buffer.from(voronization(voronoi, baseImageBuffer, x, width, height, Math.floor(points/2))), {
     		raw: {
       			width,
       			height,
