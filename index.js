36e09c4732cdddf95ce555351152c80524ff7705
Raffa Salisbury <raffasalisbury@gmail.com>
2026-09-26 17:15:19 +1200

    ewag

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import OpenSeadragon from "https://cdn.jsdelivr.net/npm/openseadragon@4.1.0/+esm";
+var ctx
+window.onload = startcanvas;
+function startCanvas(){
+	ctx=document.getElementById("myCanvas").getContext("2d")
+}
 
 document.addEventListener("DOMContentLoaded", () => {
   const viewer = OpenSeadragon({
