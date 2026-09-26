f9c13bff238474e0547b08e11af80fc08d8f7967
Woo <150756291+WooRepo@users.noreply.github.com>
2026-09-26 17:29:39 +1200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
 import OpenSeadragon from "https://cdn.jsdelivr.net/npm/openseadragon@4.1.0/+esm";
-var ctx
-window.onload = startCanvas;
-function startCanvas(){
-	ctx=document.getElementById("myCanvas").getContext("2d")
-}
-
 document.addEventListener("DOMContentLoaded", () => {
   const viewer = OpenSeadragon({
     id: "openseadragon-viewer",
